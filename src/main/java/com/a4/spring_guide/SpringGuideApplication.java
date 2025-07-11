a7c1897894663667b3e43cdad6f03f226080fc55
triopix <archismannath380@gmail.com>
2025-07-11 15:47:05 -0400

    Commit - Spring Initializr files

---
+++ src/main/java/com/a4/spring_guide/SpringGuideApplication.java
@@ -0,0 +1,13 @@
+package com.a4.spring_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a4/spring_guide/SpringGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a4.spring_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
